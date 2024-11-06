03eefdeacc51ae0c9b5b4f5da7d01d54ac9f94f7
isa <152889043+isabellepires3@users.noreply.github.com>
2024-11-06 13:17:13 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Profile from './components/Profile'
+
+export default function App() {
+  return( 
+        <div>
+          <Profile 
+           img={'./foto de brz.png'}
+           name={'Isabelle Marinho Pires'} 
+           bio={'Estou cursando 2º Série em Desenvolvimento de Sistemas'} 
+           contact={'+ 55 (85) 98676-7995'} 
+           email={'marinhoisabellemarinhopires@gmail.com'} 
+           githubUrl={'https://github.com/isabellepires3'} 
+           instagramUrl={'https://instagram.com/brz_isabelle'} 
+           />
+        </div> 
+   
+  )
+}
